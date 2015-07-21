bfdbf300c15d64b63044f5dac9248413b345eede
Adrien Lecharpentier <adrien.lecharpentier@gmail.com>
2015-07-21 17:29:48 +0200

    Update even HTTP API to upload images into events

--- src/main/java/fr/alecharp/picshare/http/EventController.java
+++ src/main/java/fr/alecharp/picshare/http/EventController.java
@@ -1,13 +1,20 @@
 package fr.alecharp.picshare.http;
 
 import fr.alecharp.picshare.domain.Event;
+import fr.alecharp.picshare.domain.Picture;
 import fr.alecharp.picshare.service.EventService;
+import fr.alecharp.picshare.service.PictureService;
+import net.codestory.http.Request;
 import net.codestory.http.annotations.Post;
 import net.codestory.http.annotations.Prefix;
+import net.codestory.http.annotations.Put;
 import net.codestory.http.payload.Payload;
 
 import javax.inject.Inject;
 import java.util.Optional;
+import java.util.Set;
+
+import static java.util.stream.Collectors.toSet;
 
 /**
  * @author Adrien Lecharpentier
@@ -15,10 +22,12 @@ import java.util.Optional;
 @Prefix("/api/event")
 public class EventController {
     private final EventService eventService;
+    private final PictureService pictureService;
 
     @Inject
-    public EventController(EventService eventService) {
+    public EventController(EventService eventService, PictureService pictureService) {
         this.eventService = eventService;
+        this.pictureService = pictureService;
     }
 
     @Post
@@ -26,5 +35,19 @@ public class EventController {
         Optional<Event> save = eventService.save(event);
         return save.isPresent() ? Payload.created("/api/event/" + save.get().id()) :
                 Payload.badRequest();
+    }
+
+    @Put("/:id/pictures")
+    public Payload attachPicture(String id, Request req) {
+        Set<Picture> pictures = req.parts().stream()
+                .filter(part -> part.name().equals("picture"))
+                .map(part -> pictureService.upload(id, part))
+                .filter(Optional::isPresent).map(Optional::get)
+                .collect(toSet());
+        Optional<Event> event = eventService.attachPictures(id, pictures);
+        if (!event.isPresent()) {
+            return Payload.notFound();
+        }
+        return Payload.ok().withHeader("Location", "/api/event/" + event.get().id());
     }
 }
